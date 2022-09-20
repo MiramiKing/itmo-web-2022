59360c20f5a048ea6460883d512257278519c03f
misha <mishaivanov99@gmail.com>
2022-09-20 18:10:33 +0300

    task 1 2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const plaint = () => null;
 
 export const getServerSideProps = async ({res}) => {
   res.setHeader('Access-Control-Allow-Origin','*')
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
   res.setHeader('X-Author', 'i_mikhael')
   res.end('i_mikhael');
   return {props: {}};
